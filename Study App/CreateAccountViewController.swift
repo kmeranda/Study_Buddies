6ee1837e4d30760ad8603debdf7a02c5bd1c556f
Kelsey Meranda <kmeranda@nd.edu>
2017-03-10 15:12:51 -0500

    separate login and create account into two different views. add profile view and naviagation view

--- Study App/CreateAccountViewController.swift
+++ Study App/CreateAccountViewController.swift
@@ -1,37 +1,35 @@
 //
-//  ViewController.swift
-//  Study App
+//  CreateAccountViewController.swift
+//  Pods
 //
 //  Created by Kelsey Meranda on 3/8/17.
-//  Copyright © 2017 Kelsey Meranda. All rights reserved.
+//
 //
 
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class CreateAccountViewController: UIViewController {
 
-    /* LoginView Controller */
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordConfirmationField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var gradYearField: UITextField!
+    @IBOutlet weak var classNameField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let user = FIRAuth.auth()?.currentUser { // user already logged in
-            self.performSegue(withIdentifier: "LoggingIn", sender: user)
-        }
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func createAccountAction(_ sender: UIButton) {}   // for segue
-
     
-    @IBAction func loginAction(_ sender: UIButton) {
-        
+    @IBAction func createAccountAction(_ sender: UIButton) {
         if self.emailField.text == "" || self.passwordField.text == "" {
             
             let alertController = UIAlertController(title: "Oops!", message: "Please make sure to enter an email and password before trying to make an account.", preferredStyle: .alert)
@@ -41,31 +39,20 @@ class ViewController: UIViewController {
             
         } else {
             
-            FIRAuth.auth()?.signIn(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: { (user, error) in
+            FIRAuth.auth()?.createUser(withEmail: self.emailField.text!, password: self.passwordField.text!, completion: {(user, error) in
                 if error == nil {
                     self.performSegue(withIdentifier: "LoggingIn", sender: nil)
-                    
                 } else {
-                    
                     let alertController = UIAlertController(title: "Oops!", message: error?.localizedDescription, preferredStyle: .alert)
                     let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
                     alertController.addAction(defaultAction)
                     self.present(alertController, animated: true, completion: nil)
-                    
                 }
             })
             
         }
-
     }
-    
-    /* @IBAction func logoutAction(_ sender: UIButton) {
-        try! FIRAuth.auth()?.signOut()
-        self.usernameLabel.text = ""
-        self.logoutButton.alpha = 0.0
-        self.emailField.text = ""
-        self.passwordField.text = ""
-    } */
-    
-}
 
+    @IBAction func logInAction(_ sender: Any) {
+    }
+}
