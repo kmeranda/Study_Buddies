16989019746f952edcd08a4ccb60126cfcc1dcf3
Kelsey Meranda <kmeranda@nd.edu>
2017-05-02 01:22:50 -0400

    study sessions now being pulled from the Firebase database and put as pin on the map

--- Study App/MapViewController.swift
+++ Study App/MapViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import MapKit
 import CoreLocation
+import Firebase
 
 class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
@@ -35,9 +36,50 @@ class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManager
         
         let location = locations.last
         let center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
         self.mapView.setRegion(region, animated: true)
+        //self.mapView.showsBuildings = true
+        self.mapView.mapType = MKMapType.hybrid
         self.locationManager.stopUpdatingLocation()
+        
+        // get array of study sessions
+        // get profile info
+        var ref: FIRDatabaseReference!
+        ref = FIRDatabase.database().reference()
+        
+        ref.child("sessions").observe(.childAdded, with: { (snapshot) -> Void in
+        //ref.child("sessions").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let val = snapshot.value as? NSDictionary
+            print("purpose: " + String(describing: val?["purpose"]))
+            //let username = value?["display_name"] as? String ?? ""
+            //for sess in snapshot.children {
+                //let sess_dict = snapshot.childSnapshotForPath((sess).key).value
+                //print(sess)
+            //}
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: val?["latitude"]! as! CLLocationDegrees, longitude: val?["longitude"]! as! CLLocationDegrees)
+            annotation.title = val?["group"]! as! String
+            annotation.subtitle = val?["purpose"]! as! String
+            self.mapView.addAnnotation(annotation)
+
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+
+        
+        //var arr = [NSDictionary]()
+        //let x : NSDictionary = NSDictionary()
+        //x = {"group": "Study Buddies", "purpose": "Studying Mobile Computing", "latitude": 41.699755, "longitude": -8623716, "time": 1493669925 }
+        //arr.append(x)
+        
+        // add pins of study sessions
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 41.699755, longitude: -86.23716)
+        annotation.title = "Study Buddies"
+        annotation.subtitle = "Studying Mobile Computing"
+        self.mapView.addAnnotation(annotation)
         
     }
     
